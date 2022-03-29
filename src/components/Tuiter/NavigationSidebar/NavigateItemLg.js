dbbdb5b0dd6ac85194063f8bb69205be86878934
danagajewski <danagajewski2018@gmail.com>
2022-03-28 23:35:31 -0400

    added buttons for navigvation

--- src/components/Tuiter/NavigationSidebar/NavigateItemLg.js
+++ src/components/Tuiter/NavigationSidebar/NavigateItemLg.js
@@ -1,14 +1,20 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
 const NavigateItemLg = ({
   nav = {
-    icon : "fa-brands fa-twitter",
-    content : "",
-    active : true
+    icon: "fa-brands fa-twitter",
+    content: "",
+    active: true
   }
 }) => {
   return (
-              <button className={`list-group-item ${nav.active ? ' list-group-item-action active' : ' list-group-item-action'}`}>
-                <i className={`${nav.icon}`}/> {nav.content}
-              </button>
-    );
+      <Link to={nav.link}>
+        <button className={`list-group-item ${nav.active
+            ? ' list-group-item-action active' : ' list-group-item-action'}`}>
+          <i className={`${nav.icon}`}/> {nav.content}
+        </button>
+      </Link>
+  );
 }
 export default NavigateItemLg;
--- src/components/Tuiter/NavigationSidebar/NavigateItemSm.js
+++ src/components/Tuiter/NavigationSidebar/NavigateItemSm.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const NavigateItemSm = ({
-    nav = {
-      "icon" : "fa-brands fa-twitter",
-      "content" : "",
-      "active" : true
-    }
+  nav = {
+    "icon": "fa-brands fa-twitter",
+    "content": "",
+    "active": true
+  }
 }) => {
   return (
-    <button className="list-group-item list-group-item-action">
-      <i className={`${nav.icon}`}/>
-    </button>
+      <Link to={nav.link}>
+        <button className="list-group-item list-group-item-action">
+          <i className={`${nav.icon}`}/>
+        </button>
+      </Link>
   );
 }
 export default NavigateItemSm;
